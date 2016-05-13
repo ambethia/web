69e8b82b95a08fd3b32b0ba9353b0052bdfc6085
Jason L Perry <jasper@ambethia.com>
2016-05-13 03:06:11 -0400

    Hello, Ambethia

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: "./index",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+};
